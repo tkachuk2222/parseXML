62602f2d92266c235d9d8f58cc0aa833ddd99109
Tkachuk Ivan <tkachyk2222@gmail.com>
2017-04-12 00:43:53 +0300

    second commit

--- workWithXML/Program.cs
+++ workWithXML/Program.cs
@@ -10,80 +10,118 @@ namespace workWithXML
 {
     class Program
     {
-        private const string filename = "..//..//..//currency-cash.xml";
+        private const string filename = "http://resources.finance.ua/ua/public/currency-cash.xml";
         static private int cnt = 0;
         static void Main(string[] args)
         {
             XPathDocument doc = new XPathDocument(filename);
             XPathNavigator nav = doc.CreateNavigator();
-
-            XPathNodeIterator it =  nav.Select("/source/organizations/organization");
             //
-            //iterators for exchange
-            XPathNodeIterator valuta = nav.Select("/source/organizations/organization/currencies");
-
-            while (it.MoveNext())
+            //creating xml writer
+            XmlTextWriter writer = null;
+            try
             {
                 //
-                //creating iterators for valutes, banks, and adress
-                valuta.MoveNext();
-                XPathNodeIterator itID = valuta.Current.Select("c/@id");
-                XPathNodeIterator itBR = valuta.Current.Select("c/@br");
-                XPathNodeIterator itAR = valuta.Current.Select("c/@ar");
-
-                XPathNodeIterator itName =  it.Current.Select("title/@value");
-                XPathNodeIterator itAdress = it.Current.Select("address/@value");
-
+                //initializing writer
+                writer = new XmlTextWriter("Banks.xml", System.Text.Encoding.Unicode);
                 //
-                //reading name of all banks and they adress
-                itName.MoveNext();
-                itAdress.MoveNext();
-                Console.WriteLine($"{itName.Current.Value}\t\t{itAdress.Current.Value}");
+                //choose formating
+                writer.Formatting = Formatting.Indented;
                 //
-                //reading exchange rate
-                while (itID.MoveNext())
-                {
-                    itBR.MoveNext();
-                    itAR.MoveNext();
-                    Console.WriteLine($"{itID.Current.Value}\t{itBR.Current.Value}\t{itAR.Current.Value}");
-                }
-                Console.WriteLine($"\t\t");
-            }
-
-
-
-            Console.WriteLine("\n\n\t\tExchange offices\n");
-            it = nav.Select("/source/organizations/organization[@org_type=2]");
-            valuta = nav.Select("/source/organizations/organization/currencies");
-            while (it.MoveNext())
-            {
+                //writing "xml ololo ver=1.0 ololo"
+                writer.WriteStartDocument();
                 //
-                //creating iterators for valutes, banks, and adress
-                //
-                //creating iterators for valutes, banks, and adress
-                valuta.MoveNext();
-                XPathNodeIterator itID = valuta.Current.Select("c/@id");
-                XPathNodeIterator itBR = valuta.Current.Select("c/@br");
-                XPathNodeIterator itAR = valuta.Current.Select("c/@ar");
+                //creating tegs 
+                writer.WriteStartElement("Banks");
 
-                XPathNodeIterator itName = it.Current.Select("title/@value");
-                XPathNodeIterator itAdress = it.Current.Select("address/@value");
+                XPathNodeIterator it = nav.Select("/source/organizations/organization");
                 //
-                //reading name of all banks and they adress
+                //iterators for exchange
+                XPathNodeIterator valuta = nav.Select("/source/organizations/organization/currencies");
                 
-                itName.MoveNext();
-                itAdress.MoveNext();
-                Console.WriteLine($"{itName.Current.Value}\t\t{itAdress.Current.Value}");
-                //
-                //reading exchange rate
-                while (itID.MoveNext())
+                while (it.MoveNext())
                 {
-                    itBR.MoveNext();
-                    itAR.MoveNext();
-                    Console.WriteLine($"{itID.Current.Value}\t{itBR.Current.Value}\t{itAR.Current.Value}");
+                    writer.WriteStartElement("bank");
+
+                    //
+                    //creating iterators for valutes, banks, and adress
+                    valuta.MoveNext();
+                    XPathNodeIterator itID = valuta.Current.Select("c/@id");
+                    XPathNodeIterator itBR = valuta.Current.Select("c/@br");
+                    XPathNodeIterator itAR = valuta.Current.Select("c/@ar");
+
+                    XPathNodeIterator itName = it.Current.Select("title/@value");
+                    XPathNodeIterator itAdress = it.Current.Select("address/@value");
+
+                    //
+                    //reading name of all banks and they adress
+                    itName.MoveNext();
+                    itAdress.MoveNext();
+                    Console.WriteLine($"{itName.Current.Value}\t\t{itAdress.Current.Value}");
+
+
+                    writer.WriteElementString("name", itName.Current.Value);
+                    writer.WriteElementString("adress", itAdress.Current.Value);
+
+
+                    //
+                    //reading exchange rate
+                    writer.WriteStartElement("currencies");
+                    while (itID.MoveNext())
+                    {
+                        itBR.MoveNext();
+                        itAR.MoveNext();
+                        Console.WriteLine($"{itID.Current.Value}\t{itBR.Current.Value}\t{itAR.Current.Value}");
+                        //writer.WriteElementString("c", itName.Current.Value);
+                    }
+                    writer.WriteEndElement();
+
+                    Console.WriteLine($"\t\t");
+                    writer.WriteEndElement();   //bank
                 }
-                Console.WriteLine($"\t\t");
+
+
+
+                Console.WriteLine("\n\n\t\tExchange offices\n");
+                it = nav.Select("/source/organizations/organization[@org_type=2]");
+                valuta = nav.Select("/source/organizations/organization/currencies");
+                while (it.MoveNext())
+                {
+                    //
+                    //creating iterators for valutes, banks, and adress
+                    //
+                    //creating iterators for valutes, banks, and adress
+                    valuta.MoveNext();
+                    XPathNodeIterator itID = valuta.Current.Select("c/@id");
+                    XPathNodeIterator itBR = valuta.Current.Select("c/@br");
+                    XPathNodeIterator itAR = valuta.Current.Select("c/@ar");
+
+                    XPathNodeIterator itName = it.Current.Select("title/@value");
+                    XPathNodeIterator itAdress = it.Current.Select("address/@value");
+                    //
+                    //reading name of all banks and they adress
+
+                    itName.MoveNext();
+                    itAdress.MoveNext();
+                    Console.WriteLine($"{itName.Current.Value}\t\t{itAdress.Current.Value}");
+                    //
+                    //reading exchange rate
+                    while (itID.MoveNext())
+                    {
+                        itBR.MoveNext();
+                        itAR.MoveNext();
+                        Console.WriteLine($"{itID.Current.Value}\t{itBR.Current.Value}\t{itAR.Current.Value}");
+                    }
+                    Console.WriteLine($"\t\t");
+                }
+                writer.WriteEndElement();
             }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+            Console.ReadLine();
         }
             
         //    XmlTextReader read = null;
